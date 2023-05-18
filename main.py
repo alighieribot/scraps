2615921297e5c9b977e810a4802d5c39ca93f36e
alighieribot <pedrooliveira28064212@gmail.com>
2023-05-18 15:28:39 -0300

    beta-0.0

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import time
+import datetime
+import requests
+import csv
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+UPDATE_DELAY = 86400
 
+def get_content(element):
+    if element:
+        return element.get_text().strip()
+    else:
+        return 'NoneType'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+URL = 'https://www.amazon.com.br/HD-SSD-KINGSTON-SA400S37-480GB/dp/B075BKXSCQ/ref=sr_1_4?__mk_pt_BR=ÅMÅŽÕÑ'
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.4788.5"}
 
+def update():
+    try:
+        #get page content
+        page = requests.get(URL, headers=headers)
+        content = BeautifulSoup(page.content, "html.parser")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        title_element = content.find("span", {"id": "productTitle"})
+        title = get_content(title_element)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        price_element = content.find("span", {"class": "a-offscreen"})
+        price = get_content(price_element)
+
+        #header structure
+        today = datetime.date.today()
+        header = ['Title', 'Price', 'Date']
+        data = [title, price, today]
+        print(header)
+        print(data)
+
+        #export to csv
+        with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(data)
+
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
+    except Exception as ex:
+        print("An exception occurred:", ex)
+
+while(1):
+    update()
+    time.sleep(UPDATE_DELAY)
