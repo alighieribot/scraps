17fc4dd58ef7c73fffcf21dfa35aafd4388ae50e
alighieribot <pedrooliveira28064212@gmail.com>
2023-05-18 17:27:01 -0300

    changed the url to a list of products; implemented next_page(); implemented conversion to csv dataframe in pandas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import datetime
 import requests
-import csv
+import pandas as pd
 from bs4 import BeautifulSoup
 
 UPDATE_DELAY = 86400
@@ -12,38 +12,70 @@ def get_content(element):
     else:
         return 'NoneType'
 
-URL = 'https://www.amazon.com.br/HD-SSD-KINGSTON-SA400S37-480GB/dp/B075BKXSCQ/ref=sr_1_4?__mk_pt_BR=ÅMÅŽÕÑ'
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.4788.5"}
+url = 'https://www.amazon.com.br/s?k=ssd'
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 OPR/98.0.0.0"
+}
 
-def update():
+data_list = []  # List to store the scraped data
+
+def update(url):
     try:
-        #get page content
-        page = requests.get(URL, headers=headers)
+        # Get page content
+        page = requests.get(url, headers=headers)
         content = BeautifulSoup(page.content, "html.parser")
 
-        title_element = content.find("span", {"id": "productTitle"})
-        title = get_content(title_element)
+        products = content.find_all("div", {"data-component-type": "s-search-result"})
 
-        price_element = content.find("span", {"class": "a-offscreen"})
-        price = get_content(price_element)
+        for product in products:
+            title_element = product.find("span", {"class": "a-size-base-plus a-color-base a-text-normal"})
+            title = get_content(title_element)
 
-        #header structure
-        today = datetime.date.today()
-        header = ['Title', 'Price', 'Date']
-        data = [title, price, today]
-        print(header)
-        print(data)
+            price_element = product.find("span", {"class": "a-price-whole"})
+            price = get_content(price_element)
 
-        #export to csv
-        with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
-            writer = csv.writer(f)
-            writer.writerow(data)
+            rating_element = product.find("span", {"class": "a-icon a-icon-star-small a-star-small-5 aok-align-bottom"})
+            rating = get_content(rating_element)
+
+            url_element = product.find("a", {"class": "a-link-normal s-no-outline"})
+            product_url = url_element["href"]
+
+            today = datetime.date.today()
+
+            data_dict = {
+                'Title': title,
+                'Price': price,
+                'Rating': rating,
+                'URL': product_url,
+                'Date': today
+            }
+
+            data_list.append(data_dict)  # Add the data dictionary to the list
+
+            print(data_dict)  # Print the dictionary for verification
 
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
     except Exception as ex:
         print("An exception occurred:", ex)
 
-while(1):
-    update()
+def next_page(content):
+    next_link = content.find("a", {"class": "s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})
+    if next_link and not next_link.find("span", {"class": "s-pagination-item s-pagination-next s-pagination-disabled"}):
+        url_next = 'https://www.amazon.com.br' + next_link['href']
+        return url_next
+    else:
+        return None
+
+# Start scraping process
+current_url = url
+while current_url:
+    update(current_url)
+    current_url = next_page(BeautifulSoup(requests.get(current_url, headers=headers).content, "html.parser"))
     time.sleep(UPDATE_DELAY)
+
+# Convert the data list to a DataFrame
+df = pd.DataFrame(data_list)
+
+# Export the DataFrame to a CSV file
+df.to_csv('AmazonWebScraperDataset.csv', index=False)
